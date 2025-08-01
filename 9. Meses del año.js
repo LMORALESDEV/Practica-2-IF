87c5b741e4bb7cc4d326fe1437a252593bfbe98a
Luis Felipe Morales <145800266+LMORALESDEV@users.noreply.github.com>
2025-08-01 12:05:43 -0400

    9. Meses del año

---
+++ 9. Meses del año.js
@@ -0,0 +1,34 @@
+/*9. Meses del año
+Pide al usuario que ingrese un número del 1 al 12 y utiliza if para mostrar el nombre
+del mes correspondiente (1 es enero, 2 es febrero, etc.).
+*/
+
+let numeroMes = prompt("Ingrese un número del 1 al 12:");
+
+if (numeroMes == 1) {
+    console.log("Enero");
+}   else if (numeroMes == 2) {
+    console.log("Febrero");
+}   else if (numeroMes == 3) {
+    console.log("Marzo");
+}   else if (numeroMes == 4) {
+    console.log("Abril");
+}   else if (numeroMes == 5) {
+    console.log("Mayo");
+}   else if (numeroMes == 6) {
+    console.log("Junio");
+}   else if (numeroMes == 7) {
+    console.log("Julio");
+}   else if (numeroMes == 8) {
+    console.log("Agosto");
+}   else if (numeroMes == 9) {
+    console.log("Septiembre");
+}   else if (numeroMes == 10) {
+    console.log("Octubre");
+}   else if (numeroMes == 11) {
+    console.log("Noviembre");
+}   else if (numeroMes == 12) {
+    console.log("Diciembre");
+}   else {
+    console.log("Número inválido. Por favor, ingrese un número del 1 al 12.");
+}
