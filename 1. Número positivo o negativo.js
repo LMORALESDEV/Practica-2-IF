a087d5fc2de9aebdfe51203a9e74794b90ef16a7
Luis Felipe Morales <145800266+LMORALESDEV@users.noreply.github.com>
2025-08-01 12:26:58 -0400

    Update 1. Número positivo o negativo.js

--- 1. Número positivo o negativo.js
+++ 1. Número positivo o negativo.js
@@ -5,9 +5,12 @@ número es positivo, negativo o cero, y muestra un mensaje adecuado.*/
 let numeromixto = prompt("Ingrese un número positivo o negativo:");
 
 if (numeromixto > 0) {
+    alert("El número es positivo.");
     console.log("El número es positivo.");
 } else if (numeromixto < 0) {
+    alert("El número es negativo.");
     console.log("El número es negativo.");
 } else {
+    alert("El número es cero.");
     console.log("El número es cero.");
 }
