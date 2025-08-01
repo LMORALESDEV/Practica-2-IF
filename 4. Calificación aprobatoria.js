4e7a411fe2074ebd6f747d1367f6e7b432fe112e
Luis Felipe Morales <145800266+LMORALESDEV@users.noreply.github.com>
2025-08-01 12:27:09 -0400

    Update 4. Calificación aprobatoria.js

--- 4. Calificación aprobatoria.js
+++ 4. Calificación aprobatoria.js
@@ -4,7 +4,9 @@ aprobatoria (60 o más) o reprobatoria.*/
 
 let calificacion = prompt("Ingrese su calificación (0-100):");
 if (calificacion >= 60 && calificacion <= 100) {
+    alert("La calificación es aprobatoria.");
     console.log("La calificación es aprobatoria.");
 } else if (calificacion < 60 && calificacion >= 0) {
+    alert("La calificación es reprobatoria.");
     console.log("La calificación es reprobatoria.");
 }
