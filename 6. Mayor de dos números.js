8cf93d5962466a5859ee67fa0951047c030780ac
Luis Felipe Morales <145800266+LMORALESDEV@users.noreply.github.com>
2025-08-01 12:27:17 -0400

    Update 6. Mayor de dos números.js

--- 6. Mayor de dos números.js
+++ 6. Mayor de dos números.js
@@ -7,7 +7,9 @@ let numero1 = parseFloat(prompt("Ingrese el primer número:"));
 let numero2 = parseFloat(prompt("Ingrese el segundo número:"));
 
 if (numero1 > numero2) {
+    alert("El primer número es mayor: " + numero1);
     console.log("El primer número es mayor: " + numero1);
 }else  {
+    alert("El segundo número es mayor: " + numero2);
     console.log("El segundo número es mayor: " + numero2);
 }
